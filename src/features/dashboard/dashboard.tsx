534bdd31857078a72b6397f0de56a16937b41df4
Joao Edmundo <joao.a.edmundo@gmail.com>
2019-10-28 21:54:14 +0100

    feat: fetch data from a service

--- src/features/dashboard/dashboard.tsx
+++ src/features/dashboard/dashboard.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useObservable } from 'rxjs-hooks'
+import { PlaceholderService } from './services/placeholder.service'
 
 const Dashboard: React.FC = () => {
+  const [name, setName] = useState('')
+  const value = useObservable(PlaceholderService.getPlaceholders)
+
   return (
     <>
       <div>DASHBORD WORKS</div>
+      <div>{name}</div>
+      <button onClick={() => setName('TEST')}>SET</button>
+      <div>{JSON.stringify(value)}</div>
     </>
   )
 }
---
+++ src/features/dashboard/services/placeholder.service.ts
@@ -0,0 +1,20 @@
+import Axios, { AxiosResponse } from 'axios'
+import { from, Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+
+import { Todo } from '../../../shared/store/todos/types'
+import { CONFIGURATION } from '../../../shared/configuration/configuration'
+
+export const PlaceholderService = {
+  getPlaceholders: (): Observable<Todo[]> =>
+    from(Axios.get(CONFIGURATION.todos.fetchTodos)).pipe(
+      map((result: AxiosResponse) => parseTodos(result))
+    ),
+}
+
+function parseTodos(result: AxiosResponse): Todo[] {
+  return result.data.map((todo: any) => ({
+    id: todo.id,
+    title: todo.title,
+  }))
+}
